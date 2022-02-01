93dfa3c467ed8990c26a6747b18b510f602a71e6
Stephen Vaz <54773712+stephenvaz03@users.noreply.github.com>
2022-02-01 23:01:23 +0530

    random roll function is complete

--- app/src/main/java/dev/stephen/diceroller/MainActivity.kt
+++ app/src/main/java/dev/stephen/diceroller/MainActivity.kt
@@ -2,10 +2,31 @@ package dev.stephen.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val result: TextView = findViewById(R.id.textView)
+        result.text = dice.roll().toString()
+    }
+
+
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
